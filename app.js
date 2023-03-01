11227c713b735fd2a7c95219c946070dad5560c0
Aybike <100150878+aybikedalbul@users.noreply.github.com>
2023-03-01 18:34:08 +0300

    Commit 3.1.23

--- app.js
+++ app.js
@@ -1,26 +1,44 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
 const path = require('path');
+const ejs = require('ejs');
+const Photo = require('./models/Photo');
 
-const myLogger = (req, res, next) => {
-  console.log('Middleware Log 1');
-  next();
-};
-const myLogger2 = (req, res, next) => {
-  console.log('Middleware Log 2');
-  next();
-};
+const app = express();
 
-app.use(express.static('public'));
-app.use(myLogger);
-app.use(myLogger2);
+// connect DB
+mongoose.connect("mongodb://127.0.0.1:27017/pcat-test-db");
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+// Template Engine
+app.set('view engine', 'ejs');
+
+//Middlewares
+app.use(express.static('public')),
+  //Aldığımız requesti sonlandırmamızı sağladı.
+  app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routes
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({}); //Takes photos from database
+  res.render('index', {
+    photos, //Photos taken from the database are sent to the homepage
+  });
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add', (req, res) => {
+  res.render('add')
+})
+
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body);
+  res.redirect('/'); // Sends it to the homepage
 });
 
 const port = 3001;
-
 app.listen(port, () => {
   console.log(`Server started at ${port}`);
 });
